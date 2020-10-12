7e9cf24393933e6ecadc6705ba7e82cbfe7c71f6
Antoine07 <antoine.lucsko@gmail.com>
2020-10-12 12:37:04 +0200

    correction ttc

--- part1_introduction/Exercices/ttc.js
+++ part1_introduction/Exercices/ttc.js
@@ -2,18 +2,22 @@
 
 function ttc(price, tva = .2) {
 
-    if ( isNaN(parseInt(price)) ) return "Error number price value";
+    // vérification du type avec parseFloat pour les chiffres avec virgule ou sans virgule
+    if ( isNaN(parseFloat(price)) || isNaN(parseFloat(tva)) ) return "Error number price value";
 
-    return price * (1 + tva); // HT * 1.2
+    return Math.floor( ( price * (1 + tva) ) * 100 ) / 100 ; // deux chiffres significatifs après la virgule
 }
 
 console.log(ttc(100));
 console.log(ttc(100, .3));
-
-// vérifier le type 
-console.log( isNaN(parseInt(100)) === true  );
-console.log( isNaN(parseInt("Hello")) === true  );
-
 console.log(ttc("hello", .3));
 // JS est capable de transtyper une variable 
-console.log(ttc("100", .3));
+
+console.log(ttc("100.9", .3));
+// vérifier le type 
+console.log( isNaN(parseFloat(100)) === true  );
+console.log( isNaN(parseFloat("Hello")) === true  );
+
+
+console.log(ttc("100", ".3"));
+console.log(ttc(ttc(100.50, 0.2)));
