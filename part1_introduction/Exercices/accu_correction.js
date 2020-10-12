85c8209d537e2d17012d7bee73beb48a7abc9f85
Antoine07 <antoine.lucsko@gmail.com>
2020-10-12 14:09:28 +0200

    accu illustration

--- part1_introduction/Exercices/accu_correction.js
+++ part1_introduction/Exercices/accu_correction.js
@@ -14,8 +14,22 @@ function accumulator(numbers, acc = 0) {
     acc = acc + numbers.shift();
 
     // console.log(acc);
-
     return accumulator(numbers, acc); // stack overflow
 }
 
 console.log('resultat final', accumulator(numbers)); // doit retourner 55
+
+/*
+numbers = [1, 2]
+accumulator([ 1, 2], acc = 0)
+    return accumulator([2], acc = 1)
+        return accumulator([], acc = 3)
+            return 3
+
+1. return return return 3
+2. return return 3
+3. return 3
+4. 3
+
+console.log('resultat final', accumulator(numbers));
+*/
